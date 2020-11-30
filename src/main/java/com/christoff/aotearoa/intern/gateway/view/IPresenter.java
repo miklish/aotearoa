e715e6a8d04132c40dfd3d3edcf4845b87efef7c
Mike Christoff <michael.n.christoff@gmail.com>
2020-11-30 01:54:51 -0500

    Added additional presenter items for new token properties and parsing

--- src/main/java/com/christoff/aotearoa/intern/gateway/view/IPresenter.java
+++ src/main/java/com/christoff/aotearoa/intern/gateway/view/IPresenter.java
@@ -2,6 +2,7 @@ package com.christoff.aotearoa.intern.gateway.view;
 
 public interface IPresenter
 {
+    LogLevel TRACE = LogLevel.TRACE;
     LogLevel DEBUG = LogLevel.DEBUG;
     LogLevel WARN = LogLevel.WARN;
     LogLevel INFO = LogLevel.INFO;
@@ -13,4 +14,14 @@ public interface IPresenter
     void persistingValuesEnd();
     void emptyKeystore(String keystoreName);
     void templateFileNotFoundOrMalformed(String templateFile);
+
+    void collectingMetadataFromTemplates(String templateFolder);
+    void loadingTemplate(String templateName);
+    void noTokensFoundInTemplate(String templateName);
+    void mergingTokenFromTemplate(String templateName);
+    void tokenFound(String token);
+    void tokenAlreadyExists(String tokenName);
+    void tokenAdded(String tokenName);
+    void tokenHasNoProperties();
+    void tokenHasProperty(String s);
 }
