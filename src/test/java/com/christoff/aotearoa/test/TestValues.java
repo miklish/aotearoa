e021e8d3eb1cb42d7aba387ca7c9315e92c1d64c
Mike Christoff <michael.n.christoff@gmail.com>
2021-11-26 16:21:16 -0500

    added a new unit test, to test that the AO can handle special characters now like $ symbols

--- src/test/java/com/christoff/aotearoa/test/TestValues.java
+++ src/test/java/com/christoff/aotearoa/test/TestValues.java
@@ -69,6 +69,47 @@ public class TestValues
     }
 
     /***
+     * Tests that AO can handle replacement text with symbols such as '$' in them.
+     * Added: 2.0.20
+     */
+    @Test
+    public void testValuesWithSpecialChars()
+    {
+        Path templatFolderPath = Paths.get("src","test","resources", CONFIG_FOLDER, "03.service-config");
+        String templateFolder = templatFolderPath.toFile().getAbsolutePath();
+
+        Path outputFolderPath = Paths.get("src","test","resources", CONFIG_FOLDER, "03.service-config-out");
+        String outputFolder = outputFolderPath.toFile().getAbsolutePath();
+
+        Path metadataFilenamePath = Paths.get("src","test","resources", CONFIG_FOLDER, "00.A.shared","_metadata.yml");
+        String metadataFilename = metadataFilenamePath.toFile().getAbsolutePath();
+
+        Path valuesFilenamePath = Paths.get("src","test","resources", CONFIG_FOLDER, "00.A.shared","_values_special_symbols.yml");
+        String valuesFilename = valuesFilenamePath.toFile().getAbsolutePath();
+
+        List exts = new LinkedList();
+        exts.add("yml");
+        exts.add("xml");
+
+        ValueInjectRequest rq = new ValueInjectRequest();
+        rq.metadataLoc = metadataFilename;
+        rq.outputDir = outputFolder;
+        rq.templateDir = templateFolder;
+        rq.configValsLoc = valuesFilename;
+        rq.templateFileExtensions = exts;
+
+        ValueInjectResponse resp = null;
+        try {
+            resp = new ValueInjectInteractor(rq).exec();
+        } catch(ConfigException c) {
+            throw new AssumptionViolatedException("ERROR: " + c.getMessage());
+        }
+
+        // Process response
+        Assert.assertTrue(resp.resultCode.equals(ValueInjectResponse.SUCCESS));
+    }
+
+    /***
      * CONSUL_URL default value (from _metadata.yml) should be inserted in consul.yml
      * E.g.: in consul.yml, should see: consulUrl: "https://consul.com"
      */
